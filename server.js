a9c165cf1c4c9b70444d44ea7dbfa1be6d3259eb
jasoncp <24683452+jasoncp@users.noreply.github.com>
2018-02-19 11:41:33 -0800

    Initial Commit, node server setup - JP

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>App working!</h1>')
+});
+
+app.listen(PORT, () => {
+    console.log('Server\'s Running!');
+});
